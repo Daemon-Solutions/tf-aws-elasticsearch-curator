382a2fa646ead2600e1080329ec968c4054e53f4
Raymond Butcher <ray.butcher@claranet.uk>
2019-05-24 11:39:10 +0100

    Use lambda module 0.12.0 to fix concurrency bug
    As per https://github.com/claranet/terraform-aws-lambda/pull/39 if a recent version of the AWS provider is used and that fix is not in place then the result is that the Lambda function is disabled. Upgrading the Lambda module makes this module work with recent AWS provider versions.

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,5 @@
 module "lambda" {
-  source = "github.com/claranet/terraform-aws-lambda?ref=v0.10.0"
+  source = "github.com/claranet/terraform-aws-lambda?ref=v0.12.0"
 
   function_name = "${var.name}"
   description   = "Elasticsearch Curator"
